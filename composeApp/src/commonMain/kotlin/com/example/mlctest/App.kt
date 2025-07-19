4d17ae5974de253f6b84eb18652b9f2c4b60d518
michalgwo <michalgw98@gmail.com>
2025-07-19 16:45:59 +0200

    camera animation bug

--- composeApp/src/commonMain/kotlin/com/example/mlctest/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/mlctest/App.kt
@@ -1,11 +1,18 @@
 package com.example.mlctest
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.unit.dp
 import dev.sargunv.maplibrecompose.compose.MaplibreMap
 import dev.sargunv.maplibrecompose.compose.layer.LineLayer
 import dev.sargunv.maplibrecompose.compose.rememberCameraState
@@ -13,7 +20,10 @@ import dev.sargunv.maplibrecompose.compose.rememberStyleState
 import dev.sargunv.maplibrecompose.compose.source.rememberGeoJsonSource
 import dev.sargunv.maplibrecompose.core.BaseStyle
 import dev.sargunv.maplibrecompose.core.source.GeoJsonData
+import io.github.dellisd.spatialk.geojson.Position
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import kotlin.time.Duration.Companion.milliseconds
 
 @Composable
 @Preview
@@ -25,7 +35,7 @@ fun App() {
             MaplibreMap(
                 cameraState = cameraState,
                 styleState = styleState,
-                baseStyle = BaseStyle.Uri("https://api.protomaps.com/styles/v5/light/en.json?key=73c45a97eddd43fb"),
+                baseStyle = remember { BaseStyle.Uri("https://api.protomaps.com/styles/v5/light/en.json?key=73c45a97eddd43fb") },
             ) {
                 LineLayer(
                     id = "asdf",
@@ -37,6 +47,31 @@ fun App() {
             Text(
                 modifier = Modifier.align(Alignment.TopStart),
                 text = cameraState.position.zoom.toString()
+            )
+            val scope = rememberCoroutineScope()
+            val keyboardController = LocalSoftwareKeyboardController.current
+            TextField(
+                modifier = Modifier.align(Alignment.TopStart).padding(top = 20.dp),
+                value = "",
+                onValueChange = {}
+            )
+            Button(
+                modifier = Modifier.align(Alignment.TopEnd).padding(top = 20.dp),
+                onClick = {
+                    scope.launch {
+                        cameraState.animateTo(
+                            finalPosition = cameraState.position.copy(
+                                target = Position(19.938830100326083, 50.061658517346814),
+                                zoom = 13.0
+                            ),
+                            duration = 1000.milliseconds
+                        )
+                    }
+                    keyboardController?.hide()
+                },
+                content = {
+                    Text("Animate camera")
+                }
             )
         }
     }
