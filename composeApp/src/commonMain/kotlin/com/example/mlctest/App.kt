b3952117548b808d7334003def4d87b498ce8a09
michalgwo <michalgw98@gmail.com>
2025-07-25 12:48:06 +0200

    dropdown bug

--- composeApp/src/commonMain/kotlin/com/example/mlctest/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/mlctest/App.kt
@@ -1,78 +1,85 @@
 package com.example.mlctest
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import dev.sargunv.maplibrecompose.compose.MaplibreMap
-import dev.sargunv.maplibrecompose.compose.layer.LineLayer
-import dev.sargunv.maplibrecompose.compose.rememberCameraState
-import dev.sargunv.maplibrecompose.compose.rememberStyleState
-import dev.sargunv.maplibrecompose.compose.source.rememberGeoJsonSource
-import dev.sargunv.maplibrecompose.core.BaseStyle
-import dev.sargunv.maplibrecompose.core.source.GeoJsonData
-import io.github.dellisd.spatialk.geojson.Position
-import kotlinx.coroutines.launch
+import androidx.compose.ui.window.PopupProperties
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import kotlin.time.Duration.Companion.milliseconds
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        val cameraState = rememberCameraState()
-        val styleState = rememberStyleState()
-        Box {
-            MaplibreMap(
-                cameraState = cameraState,
-                styleState = styleState,
-                baseStyle = remember { BaseStyle.Uri("https://api.protomaps.com/styles/v5/light/en.json?key=73c45a97eddd43fb") },
-            ) {
-                LineLayer(
-                    id = "asdf",
-                    source = rememberGeoJsonSource(
-                        GeoJsonData.Uri("https://raw.githubusercontent.com/datanews/amtrak-geojson/refs/heads/master/amtrak-combined.geojson")
-                    )
-                )
-            }
-            Text(
-                modifier = Modifier.align(Alignment.TopStart),
-                text = cameraState.position.zoom.toString()
-            )
-            val scope = rememberCoroutineScope()
-            val keyboardController = LocalSoftwareKeyboardController.current
-            TextField(
-                modifier = Modifier.align(Alignment.TopStart).padding(top = 20.dp),
-                value = "",
-                onValueChange = {}
-            )
-            Button(
-                modifier = Modifier.align(Alignment.TopEnd).padding(top = 20.dp),
-                onClick = {
-                    scope.launch {
-                        cameraState.animateTo(
-                            finalPosition = cameraState.position.copy(
-                                target = Position(19.938830100326083, 50.061658517346814),
-                                zoom = 13.0
-                            ),
-                            duration = 1000.milliseconds
-                        )
-                    }
-                    keyboardController?.hide()
-                },
-                content = {
-                    Text("Animate camera")
+        val isExpanded = remember { mutableStateOf(false) }
+        val density = LocalDensity.current
+        var boxHeight by remember { mutableStateOf(0.dp) }
+        var clickedPosition by remember { mutableStateOf(DpOffset.Unspecified) }
+
+        Box(Modifier.fillMaxSize()) {
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Green)
+                .pointerInput(Unit) {
+                    detectTapGestures(onTap = {
+                        with(density) {
+                            val x = it.x.toDp()
+                            val y = it.y.toDp()
+                            clickedPosition = DpOffset(x, y)
+                            isExpanded.value = true
+                        }
+                    })
                 }
-            )
+                .onSizeChanged {
+                    with(density) {
+                        val size = DpSize(it.width.toDp(), it.height.toDp())
+                        boxHeight = size.height
+                    }
+                },
+            ) {}
+            SampleMenu(isExpanded, boxHeight, clickedPosition)
         }
+    }
+}
+
+@Composable
+fun SampleMenu(isExpanded: MutableState<Boolean>, boxHeight: Dp, clickedPosition: DpOffset) {
+    DropdownMenu(
+        expanded = isExpanded.value,
+        properties = PopupProperties(focusable = false),
+        offset = DpOffset(clickedPosition.x, clickedPosition.y-boxHeight),
+        onDismissRequest = { isExpanded.value = false }
+    ) {
+        DropdownMenuItem(
+            text = { Text(text = "1") },
+            onClick = {}
+        )
+        DropdownMenuItem(
+            text = { Text(text = "2") },
+            onClick = {}
+        )
+        DropdownMenuItem(
+            text = { Text(text = "3") },
+            onClick = {}
+        )
     }
 }
